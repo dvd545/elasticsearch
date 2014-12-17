3329d4e601914ff46ec85d4ed3429fe3f1ba3acb
Diver David <diverdavid@mac-air.home>
2014-12-16 22:39:12 -0500

    add term prelim

--- index.php
+++ index.php
@@ -28,19 +28,9 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Project name</a>
+          <a class="navbar-brand" href="#">IS421 Elastic Search Project By David Schmidt</a>
         </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <form class="navbar-form navbar-right" role="form">
-            <div class="form-group">
-              <input type="text" placeholder="Email" class="form-control">
-            </div>
-            <div class="form-group">
-              <input type="password" placeholder="Password" class="form-control">
-            </div>
-            <button type="submit" class="btn btn-success">Sign in</button>
-          </form>
-        </div><!--/.navbar-collapse -->
+        <!--/.navbar-collapse -->
       </div>
     </nav>
 
@@ -48,7 +38,7 @@
     <div class="jumbotron">
       <div class="container">
         <h1>Search With Elastic Search</h1>
-        <p>Criteria is searched from a database of tweets pulled from twitter. You can also add some terms into the database</p>
+        <p>Results are displayed from a database of tweets</p>
           <form action="index.php" method="get" autocomplete="off">
               <div class="inner-addon right-addon ">
                   <i class="glyphicon glyphicon-search"></i>
@@ -68,7 +58,6 @@
                       </script>
                      </div>
           </form>
-        <p><a class="btn btn-primary btn-lg" href="#" role="button">Add Terms &raquo;</a></p>
 
       </div>
     </div>
@@ -76,20 +65,85 @@
     <div class="container">
       <!-- Example row of columns -->
       <div class="row">
-        <div class="col-md-4">
-          <h2>Heading</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div>
-        <div class="col-md-4">
-          <h2>Heading</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-       </div>
-        <div class="col-md-4">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        <div class="col-md-12">
+          <?php
+                         require_once 'vendor/autoload.php';
+                        require_once 'tw_autoloader.php';
+                        ini_set('display_errors', 'On');
+            
+                        $es = new Elasticsearch\Client();
+
+                        if($_GET!=NULL){
+                            $q = $_GET['q'];
+                        
+
+                            $search = \Classes\curlFunction::Search($q);
+                            $array_tweet = array();
+                            $table = "<h2>Query Results</h2>
+                                        <table class='table table-striped table-hover'>
+                                          <thead>
+                                            <tr>
+                                              <th>Results</th>
+                                            </tr>
+                                          </thead>
+                                          <tbody>";
+                            if($search['hits']['total'] == 0){
+                                
+                                
+                                
+                                ?>
+                                <div class="termadding">
+                                <form action="index.php" method="post">
+                                    <?php 
+                                        $term = $_GET['q'];
+                                        echo "<button class='btn btn-primary btn-lg' type='submit' name='term'>Add                                           " . $term . " &raquo; </button>"; ?>
+                                        </form>
+                                    </div>
+                                <?php 
+                                if(isset($_POST['submit'])){
+                                    
+                                    $url = 'search/tweets.json';
+                                    $getfield = '?q=' . $term;
+                                    $settings = \Classes\Config::password();
+                                    $obj = \Classes\TwitterFunctions::get_field($url, $getfield, $settings);
+                                    foreach($obj as $items){
+                                        foreach($items as $item){
+                                            if(!empty($item)){
+                                                $val = $item['text'];
+                                                $feed_item = array("text" => $val);
+                                                $add = \Classes\curlFunction::Import($feed_item);
+                                                print_r($add);
+
+                                            }
+                                        }
+                                    }
+                                    
+                                    
+                                }
+                            }else{
+                            foreach($search['hits']['hits'] as $hits){
+                                foreach($hits['_source'] as $message){
+                                    $table .= "<tr><td>" . $message . "</td></tr>";
+
+                                }
+                            
+
+                            }
+                            $table .= "</tbody></table>";
+                            echo $table;
+
+
+                            }
+                        }
+                            
+                     
+                            
+
+                    ?>
+
+    
+
+        
         </div>
       </div>
 
